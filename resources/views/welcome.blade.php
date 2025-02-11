4cfb49867c98e9cabaed570b12fafd38d8d9cad6
benndatho <ndathoben@gmail.com>
2025-02-12 00:15:56 +0300

    updated js/css path 'Welcome blade page'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/hightlightjs-dark.css">
+{{--    <link rel="stylesheet" href="css/hightlightjs-dark.css">--}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/highlight.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,300&family=Source+Code+Pro:wght@300&display=swap" rel="stylesheet">
 {{--    <link rel="stylesheet" href="css/style.css" media="all">--}}
     <link rel="stylesheet" type="text/css"  href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" type="text/css"  href="{{ asset('assets/css/hightlightjs-dark.css') }}">
+{{--    <link rel="stylesheet" href="css/hightlightjs-dark.css">--}}
     <script>hljs.initHighlightingOnLoad();</script>
 </head>
 
