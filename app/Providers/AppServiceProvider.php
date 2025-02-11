8bf6ea4bb0ad4e82c40f6ed54b9c941d6ff6ecd9
benndatho <ndathoben@gmail.com>
2025-02-11 23:26:22 +0300

    updated AppServiceProvider with "https value"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
