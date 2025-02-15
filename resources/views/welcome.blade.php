69605bdcdfe0a93001e235cc4bb3b2e7612ab28c
benndatho <ndathoben@gmail.com>
2025-02-15 12:29:46 +0300

    updated css directory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <!-- VENDOR CSS -->
-    <link rel="stylesheet" href="{{ asset('assets-app/new/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets-app/new/vendor/font-awesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets-app/new/vendor/linearicons/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets-app/new/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets-app/new/vendor/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets-app/new/vendor/linearicons/style.css') }}">
     <!-- MAIN CSS -->
-    <link rel="stylesheet" href="{{ asset('assets-app/new/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets-app/new/css/main.css') }}">
     <!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
-    <link rel="stylesheet" href="{{ asset('assets-app/new/css/demo.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/assets-app/new/css/demo.css') }}">
     <!-- GOOGLE FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
     <!-- ICONS -->
